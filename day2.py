1389d726fb7175d19d161063f02d336e61558dff
Simant <simantchaudhary23@gmail.com>
2025-11-30 16:10:51 -0600

    day2

---
+++ day2.py
@@ -0,0 +1,13 @@
+
+full_name = "Simant Chaudhary"
+learning_per_hour = 3
+aim = "To become a professional AI Engineer"
+
+print(type(full_name))
+print(type(learning_per_hour))
+print(type(aim))
+
+#taking input from the user
+user_name = input("Enter name: ")
+
+print("Hello", user_name)
